dff28ff700a65bfdee7f284c56557fa3c34ae925
Chris Dillon <chris@ChrisMBP.local>
2020-04-15 20:43:01 +0100

    Updated LiftingStat Model

--- PowerLifting.Service/LiftingStats/DTO/LiftingStatDTO.cs
+++ PowerLifting.Service/LiftingStats/DTO/LiftingStatDTO.cs
@@ -1,10 +1,12 @@
-﻿namespace Powerlifting.Service.LiftingStats.DTO
+﻿using System;
+
+namespace Powerlifting.Service.LiftingStats.DTO
 {
     public class LiftingStatDTO
     {
         public int LiftingStatId { get; set; }
         public string UserId { get; set; }
-        public double? Percentage { get; set; }
+        public DateTime LastUpdated { get; set; }
         public double BenchWeight { get; set; }
         public double SquatWeight { get; set; }
         public double DeadliftWeight { get; set; }
--- PowerLifting.Service/LiftingStats/Model/LiftingStat.cs
+++ PowerLifting.Service/LiftingStats/Model/LiftingStat.cs
@@ -6,10 +6,9 @@ namespace Powerlifting.Service.LiftingStats.Model
     {
         public int LiftingStatId { get; set; }
         public string UserId { get; set; }
-        public double? Percentage { get; set; }
+        public DateTime LastUpdated { get; set; }
         public double BenchWeight { get; set; } = 0;
         public double SquatWeight { get; set; } = 0;
-
         public double DeadliftWeight { get; set; } = 0;
         //public virtual User User { get; set; }
     }
