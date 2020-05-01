1970f7165d87ae1d650dc2caadc516f23eb97150
Chris Dillon <chrispauldillon@live.com>
2020-05-01 20:55:41 +0100

    Decoupled mapping profiles to be in line with SoC principle

---
+++ PowerLifting.Service/UserSettings/AutoMapper/UserSettingMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using PowerLifting.Service.UserSettings.DTO;
+using PowerLifting.Service.UserSettings.Model;
+
+namespace PowerLifting.Service.UserSettings.AutoMapper
+{
+    public class UserSettingMappingProfile : Profile
+    {
+        public UserSettingMappingProfile()
+        {
+            //UserSettings
+            CreateMap<UserSetting, UserSettingDTO>();
+            CreateMap<UserSettingDTO, UserSetting>();
+        }
+    }
+}
