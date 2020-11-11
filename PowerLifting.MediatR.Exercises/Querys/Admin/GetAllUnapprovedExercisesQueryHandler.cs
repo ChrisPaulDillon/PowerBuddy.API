7575e35181596d69c59c6c0191f58a732e3b611c
Chris Dillon <chrispauldillon@live.com>
2020-11-11 16:59:22 +0000

    Added handler for moving a log day to another

--- PowerLifting.MediatR.Exercises/Querys/Admin/GetAllUnapprovedExercisesQueryHandler.cs
+++ PowerLifting.MediatR.Exercises/Querys/Admin/GetAllUnapprovedExercisesQueryHandler.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using PowerLifting.Data;
