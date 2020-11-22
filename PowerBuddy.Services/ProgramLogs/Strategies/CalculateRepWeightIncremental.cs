f2a6e548c48a7329c1f57443c554c98d709261c1
Chris Dillon <chrispauldillon@live.com>
2020-11-22 13:52:01 +0000

    Weights are now rounded to nearest 0.25 on program generation

--- PowerBuddy.Services/ProgramLogs/Strategies/CalculateRepWeightIncremental.cs
+++ PowerBuddy.Services/ProgramLogs/Strategies/CalculateRepWeightIncremental.cs
@@ -1,10 +1,12 @@
-﻿namespace PowerBuddy.Services.ProgramLogs.Strategies
+﻿using System;
+
+namespace PowerBuddy.Services.ProgramLogs.Strategies
 {
     public class CalculateRepWeightIncremental : ICalculateRepWeight
     {
         public decimal CalculateWeight(decimal weightInput, decimal baseIncrementalJump)
         {
-            return weightInput + baseIncrementalJump;
+            return Math.Round((weightInput + baseIncrementalJump) * 4, MidpointRounding.ToEven) / 4;
         }
     }
 }
