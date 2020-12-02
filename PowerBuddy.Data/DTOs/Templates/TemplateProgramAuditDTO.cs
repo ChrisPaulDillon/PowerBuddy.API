9651ce32533a0a49a05dbe35293ae2cc354a2a6b
Chris Dillon <chrispauldillon@live.com>
2020-12-02 22:47:45 +0000

    Added templateProgramId to audit dto

--- PowerBuddy.Data/DTOs/Templates/TemplateProgramAuditDTO.cs
+++ PowerBuddy.Data/DTOs/Templates/TemplateProgramAuditDTO.cs
@@ -4,6 +4,7 @@ namespace PowerBuddy.Data.DTOs.Templates
 {
     public class TemplateProgramAuditDTO
     {
+        public int TemplateProgramId { get; set; }
         public string Username { get; set; }
         public string TemplateName { get; set; }
         public DateTime DateCreated { get; set; }
