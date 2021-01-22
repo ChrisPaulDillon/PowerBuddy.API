5c6a810d563e6115504d3e331d20e799beff3ad3
Chris Dillon <chrispauldillon@live.com>
2021-01-22 14:36:51 +0000

    Added custom authorization header for determining if the user is valid or not accessing the resource

--- PowerBuddy.API/Areas/Account/Controllers/UserController.cs
+++ PowerBuddy.API/Areas/Account/Controllers/UserController.cs
@@ -67,7 +67,6 @@ namespace PowerBuddy.API.Areas.Account.Controllers
         }
 
         [HttpGet("Profile")]
-        [Authorize(AuthenticationSchemes = "Bearer")]
         [ProducesResponseType(typeof(UserDTO), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiError), StatusCodes.Status404NotFound)]
         [ProducesResponseType(typeof(ApiError), StatusCodes.Status401Unauthorized)]
@@ -131,7 +130,7 @@ namespace PowerBuddy.API.Areas.Account.Controllers
         }
 
         [HttpPut("Profile")]
-        [Authorize(AuthenticationSchemes = "Bearer")]
+        [Authorize(Policy = "IsValidUser")]
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiError), StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(typeof(ApiError), StatusCodes.Status400BadRequest)]
